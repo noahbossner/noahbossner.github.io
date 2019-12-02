bf955db3e0a3b43dbcf494851a3622a1c13b4cad
noahbossner <57044737+noahbossner@users.noreply.github.com>
2019-12-02 18:36:36 -0500

    Add files via upload

---
+++ noahbossner.github.io-master/connect.php
@@ -0,0 +1,28 @@
+<?php
+    $connectLink = mysql_connect("localhost", "root", "root", "nbossner");
+
+    if($connectLink === false){
+      die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+
+    //Creating initial variables
+        $fullName = $_POST['fullName'];
+        $emailBox = $_POST['email'];
+        $relationship = $_POST['relationship'];
+        $phoneNumber = $_POST['phoneNumber'];
+        $address = $_POST['address'];
+        $dateSent = $_POST['date'];
+        $contactTextArea = $_POST['notes'];
+
+        $sql = "INSERT INTO inquires ('fullName', 'email', 'relationship',
+        'phoneNumber', 'address', 'date', 'notes') VALUES ($fullName,
+        $email, $relationship, $phoneNumber, $address, $date, $notes)";
+
+        if($mysql_query($sql, $connectLink){
+          echo "Records have been added to database..."
+        }
+        else{
+          echo "ERROR: Could not execute $sql " . mysqli_error($connectLink)
+        }
+        $mysql_close($connectLink);
+?>
